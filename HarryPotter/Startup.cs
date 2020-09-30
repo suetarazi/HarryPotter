f926f70668770911d794ffbb61b27d407ef49f9d
Sue Tarazi <suemachtley@gmail.com>
2020-09-30 16:32:28 -0700

    proof of life with Razor Page scaffolding

--- HarryPotter/Startup.cs
+++ HarryPotter/Startup.cs
@@ -23,7 +23,8 @@ namespace HarryPotter
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            //services.AddControllersWithViews();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,6 +49,7 @@ namespace HarryPotter
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapRazorPages();
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
