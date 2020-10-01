4c8e597b05bc1757ff5d816b404fa83bc060a926
Sue Tarazi <suemachtley@gmail.com>
2020-10-01 14:18:25 -0700

    GetAllHouses method created in HouseService

--- HarryPotter/Models/HouseObjects.cs
+++ HarryPotter/Models/HouseObjects.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.Json.Serialization;
@@ -40,5 +41,7 @@ namespace HarryPotter.Models
 
         [JsonPropertyName("colors")]
         public string[] Colors { get; set; }
+
+        
     }
 }
--- HarryPotter/Models/Services/HouseService.cs
+++ HarryPotter/Models/Services/HouseService.cs
@@ -1,16 +1,41 @@
 ﻿using HarryPotter.Models.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace HarryPotter.Models.Services
 {
     public class HouseService : IHouses
     {
-        public Task<string> GetAllHouses()
+        //public string HouseName { get; set; }        
+        
+        private IConfiguration _config;
+        private readonly HttpClient _client = new HttpClient();
+        private string baseUrl = @"https://www.potterapi.com/v1";
+
+        public HouseService(IConfiguration config, HttpClient client)
         {
-            throw new NotImplementedException();
+            _config = config;
+            _client = client;
+        }
+
+        public async Task<string> GetAllHouses()
+        {
+            string route = "houses";
+
+            _client.DefaultRequestHeaders.Accept.Clear();
+            _client.DefaultRequestHeaders.Accept.Add(
+                new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            var response = await _client.GetStringAsync($"{ baseUrl }/{ route }" + _config["PotterApiKey"]);
+
+            string HouseName = JsonConvert.DeserializeObject<string>(response);
+
+            return HouseName;
         }
 
         public Task<string> GetHouseById()
