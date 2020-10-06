2aa97314f3a6f82847137269add41c751bee9227
Sue Tarazi <suemachtley@gmail.com>
2020-10-05 21:59:48 -0700

    Characters Model, Interface and Service with all methods completed

---
+++ HarryPotter/Models/Interfaces/ICharacters.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HarryPotter.Models.Interfaces
+{
+    public interface ICharacters
+    {
+        public Task<List<CharacterObject>> GetAllCharacters();
+
+        public Task<CharacterObject> GetCharacterById(string Id);
+
+        public Task<List<CharacterObject>> GetAllCharactersInAHouse(string house);
+    }
+}
